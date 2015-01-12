d103c33a5b4ce98641c433e363821eb6f0a814da
Clemens Ahrens <clemens_ahrens@gmx.de>
2015-01-12 14:48:22 +0100

    init commit

---
+++ src/Coref.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Coref {
+	ArrayList<String> inFiles;
+	String outFile;
+
+	// parser for inputs
+	void parseArgs(String[] args) {
+		Integer iIdx = null;
+		Integer oIdx = null;
+		inFiles = new ArrayList<String>();
+
+		// input must come before output ---
+		// maybe TODO: search pretty
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-i")) {
+				iIdx = new Integer(i);
+			} else if (args[i].equals("-o")) {
+				oIdx = new Integer(i);
+			}
+		}
+		if (iIdx != null & oIdx != null) {
+			outFile = args[oIdx + 1];
+			for (int i = iIdx + 1; i < oIdx; i++) {
+				inFiles.add(args[i]);
+			}
+		} else {
+			System.out.println("Usage: " + "\n"
+					+ "java Coref -i <XmlFile> -o <NewFile>" + "\n"
+					+ "Example (Unix): " + "\n"
+					+ "java Coref -i ../Input/*.xml -o ../output.xml");
+			System.exit(-1); // TODO: search exit status for failure
+		}
+	}
+
+	public static void main(String[] args) {
+		Coref coref = new Coref();
+		coref.parseArgs(args);
+
+		OutputWriter outputWriter = new OutputWriter(coref.getOutFile());
+
+		// for input file
+		try {
+			for (String inFile : coref.getInFiles()) {
+				InputAnalyzer inputAnalyzer = new InputAnalyzer(inFile);
+
+				Map<String, Coreference> coreferences = inputAnalyzer
+						.extractCoreferences();
+
+				outputWriter.addCoreferences(coreferences);
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		outputWriter.writeToFile();
+
+	}
+
+	public ArrayList<String> getInFiles() {
+		return inFiles;
+	}
+
+	public String getOutFile() {
+		return outFile;
+	}
+}
---
+++ src/InputAnalyzer.java
@@ -0,0 +1,141 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+public class InputAnalyzer {
+
+	File infile;
+
+	public InputAnalyzer(String filename) throws Exception {
+		infile = new File(filename);
+		if (!infile.exists()) {
+			throw new Exception("Fle " + filename + " not found");
+		}
+	}
+
+	public Map<String, Coreference> extractCoreferences() {
+		Map<String, Coreference> map = new HashMap<String, Coreference>();
+		// aufrufen des builders pro input
+		SAXBuilder builder = new SAXBuilder();
+		try {
+			Document doc = builder.build(infile);
+
+			// ---- Create list of <coreferences> & extract chapID
+			Element root = doc.getRootElement();
+			Element document = root.getChild("document");
+			String chapID = document.getAttribute("chap").getValue(); // extract
+																		// chapID
+																		// for
+																		// outputdoc
+			Element corefs = document.getChild("coreferences");
+			@SuppressWarnings("unchecked")
+			List<Element> listcoref = corefs.getChildren("coreference");
+			for (Element coref : listcoref) {
+				String corefID = coref.getAttribute("id").getValue();
+				@SuppressWarnings("unchecked")
+				List<Element> listmention = coref.getChildren("mention");
+				for (Element mention : listmention) {
+					if (null == mention)
+						continue;
+					String s = mention.getAttributeValue("representative"); // <AttrS
+					if (null == s || !s.equals("true"))
+						continue; // <ValS>
+					String text = mention.getChildText("text");
+					Coreference coreference = new Coreference(text, chapID, Integer.parseInt(corefID));
+					map.put(text, coreference);
+
+				}
+			}
+		} catch (JDOMException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NumberFormatException e){
+			System.err.println("Error while parsing corefID: \n" + e.getMessage());
+		}
+
+		return map;
+
+	}
+
+	public ArrayList<Element> extractCoreferences2() {
+		ArrayList<Element> arrayList = new ArrayList<Element>();
+
+		// aufrufen des builders pro input
+		SAXBuilder builder = new SAXBuilder();
+		try {
+			Document doc = builder.build(infile);
+
+			// ---- Create list of <coreferences> & extract chapID
+			Element root = doc.getRootElement();
+			Element document = root.getChild("document");
+			String chapID = document.getAttribute("chap").getValue(); // extract
+																		// chapID
+																		// for
+																		// outputdoc
+			Element corefs = document.getChild("coreferences");
+			List listcoref = corefs.getChildren("coreference");
+
+			// ---- create list of <mention> for each listcoref & extract
+			// corefID
+			for (int i = 0; i < listcoref.size(); i++) {
+				Element coref = (Element) (listcoref.get(i));
+				String corefID = coref.getAttribute("id").getValue(); // extract
+																		// corefID
+																		// for
+																		// outputdoc
+				List listmention = coref.getChildren("mention");
+
+				// Find <mention> with given attribute representative="true":
+				for (int j = 0; j < listmention.size(); j++) {
+					Element ment = (Element) (listmention.get(j));
+					if (null == ment)
+						continue;
+					String s = ment.getAttributeValue("representative"); // <AttrS
+					if (null == s || !s.equals("true"))
+						continue; // <ValS>
+					String text = ment.getChildText("text");
+
+					// test check for correct xml-element:
+					// ment.setAttribute("testattr", "testval"); //
+					// <AttrNew>=<ValNew>
+
+					// ---- create sub elements for each coref in outputdoc
+					Element newChain = new Element("chain");
+					Element newCoref = new Element("coreference");
+					Element newChapter = new Element("chapter");
+					Element newID = new Element("id");
+
+					// write results in output
+
+					newChain.setAttribute("reprText", text); // insert <text>
+																// from
+																// representative
+																// <mention> as
+																// attribute
+					newChain.addContent(newCoref);
+					newCoref.addContent(newChapter);
+					newChapter.addContent(chapID);
+					newCoref.addContent(newID);
+					newID.addContent(corefID);
+
+					arrayList.add(newChain);
+				}
+			}
+
+		} catch (JDOMException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return arrayList;
+	}
+
+}
